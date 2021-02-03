8d2138116bf72a126b655b0117c46559c9ae39c9
ajuelosemmanuel <ajuelosemmanuel@users.noreply.github.com>
2021-02-03 12:40:41 +0100

    Exercice 2 - fait !

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import exemple # Pour pouvoir utiliser les methodes de exemple.py
+import fonctions
 
-print("bonjour")
-maListe=exemple.lectureFichier("test.txt") # Execution de la methode lectureFichier du fichier exemple.
-print(maListe)
-print(len(maListe)) #Longueur de la liste.
-exemple.createFichierLP(maListe[0][0],int(maListe[1][0])) #Methode int(): transforme la chaine de caracteres en entier
+#print("bonjour")
+
+#print("LISTE DES ETU")
+listeEtu=fonctions.lectureFichierEtu("PrefEtu.txt")
+#print(listeEtu)
+
+#print("LISTE DES SPE")
+listeSpe=fonctions.lectureFichierSpe("PrefSpe.txt")
+#print(listeSpe)
+res = fonctions.gsEtu(listeEtu,listeSpe)
+print(res)
+#exemple.createFichierLP(maListe[0][0],int(maListe[1][0])) #Methode int(): transforme la chaine de caracteres en entier
